0b0f07124029c6a1d74e701424163218f103b5d3
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-27 23:40:56 +0200

    deleted unnecessary imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from './core/store';
+import App from './core/App/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
