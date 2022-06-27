e3bc8056695a241a9487f633d1c9224204dfe6d4
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-27 23:41:47 +0200

    imports new path

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from './core/store';
+import App from './core/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')!;
