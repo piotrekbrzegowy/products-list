71aa3e191ef571d72bbdc9c15138da172fd59fa1
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-06-28 12:24:17 +0200

    added reducer to store

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,10 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import createSagaMiddleware from "@redux-saga/core";
+import saga from "./saga";
+import productsSlice from '../features/ProductsList/productsSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
-    
+    products: productsSlice
   },
+  middleware: [sagaMiddleware]
 });
+
+sagaMiddleware.run(saga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
